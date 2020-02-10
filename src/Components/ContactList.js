07584fde5f437da8e7e4c4ef4717e909e09bf2c2
kel <ogbukakelechi20@gmail.com>
2020-02-10 10:06:09 +0100

    more contacts created in objects held in an array and mapped

--- src/Components/ContactList.js
+++ src/Components/ContactList.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
-
+import Contact from './Contact'
 
 class ContactList extends Component {
   render() {
     return (
       <div>
-        <ul>
-            <li>{this.props.contacts.name} {this.props.contacts.number} </li>
-        </ul>
+        {this.props.contacts.map((contacts) => {
+          return  <Contact  contacts = {contacts} key = {contacts.id}/>
+        })
+
+        }
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,11 +4,34 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-let contacts = 
+let contacts = [
   {
-    name : 'kel',
-    number : "224532"
+    id : '0',
+    name : 'kelechi',
+    phone : "909"
+  },
+  {
+    id : '1',
+    name : 'Amaka',
+    phone : "245"
+  },
+  {
+    id : '2',
+    name : 'Ngozi',
+    phone : "242"
+  },
+  {
+    id : '3',
+    name : 'Emeka',
+    phone : "544"
+  },
+  {
+    id : '4',
+    name : 'Obinna',
+    phone : "940"
   }
+
+]
 
 ReactDOM.render(<App contacts = {contacts} />, document.getElementById('root'));
 
